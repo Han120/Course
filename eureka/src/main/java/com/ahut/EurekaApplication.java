1c791006113ec68bf58db63135a80209a78f1b08
hanzijian <1204253639@qq.com>
2020-11-09 17:30:38 +0800

    1.增加eureka子模块作为注册服务端 2.将单maven项目改造成maven父子项目

--- eureka/src/main/java/com/ahut/EurekaApplication.java
+++ eureka/src/main/java/com/ahut/EurekaApplication.java
@@ -1,13 +1,11 @@
-package com.ahut.course;
+package com.ahut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
-
+public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CourseApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
-
 }
